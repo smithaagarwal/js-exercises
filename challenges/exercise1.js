271c4a2eaf075d50a2b53457065cae1723dd69fe
smithaagarwal <smitha.kotian@gmail.com>
2023-10-10 14:40:22 +0100

    feat: add the missing code to all the functions in order to make the tests pass

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,24 +1,36 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	return (person.city === 'Manchester');
+
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+	if (people%40) {
+		return (Math.floor(people/40) + 1);
+	}
+	else {
+		return (people/40);
+	}
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	let count = 0;
+	arr.forEach(element => {
+		if(element === 'sheep')
+			count++;
+	});
+	return count;
 }
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	return ((person.address.postCode.startsWith("M")) && (person.address.city==='Manchester')) 
+		
 }
