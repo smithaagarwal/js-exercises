2ce7980f5124db20d309f8c26b6b594501873fd0
smithaagarwal <smitha.kotian@gmail.com>
2023-10-11 22:58:32 +0100

    feat: update logic to all functions to use forEach

--- challenges/exercise3.js
+++ challenges/exercise3.js
@@ -1,10 +1,10 @@
 export function findSmallNums(nums) {
 	if (!nums) throw new Error('nums is required');
 	const smallerNumArr = [];
-	for (let i = 0; i < nums.length; i++) {
-		if (nums[i] < 1)
-			smallerNumArr.push(nums[i]);
-	}
+	nums.forEach(num => {
+		if (num < 1)
+			smallerNumArr.push(num);
+	});
 	return smallerNumArr;
 }
 
@@ -12,47 +12,49 @@ export function findNamesBeginningWith(names, char) {
 	if (!names) throw new Error('names is required');
 	if (!char) throw new Error('char is required');
 	const namesArr = [];
-	for (let i = 0; i < names.length; i++) {
-		if (names[i].startsWith(char))
-			namesArr.push(names[i]);
-	}
+	names.forEach(name => {
+		if (name.startsWith(char))
+			namesArr.push(name);
+	});
 	return namesArr;
 }
 
 export function findVerbs(words) {
 	if (!words) throw new Error('words is required');
 	const verbssArr = [];
-	for (let i = 0; i < words.length; i++) {
-		if (words[i].startsWith('to '))
-			verbssArr.push(words[i]);
-	}
+	words.forEach(word => {
+		if (word.startsWith('to '))
+			verbssArr.push(word);
+	});
 	return verbssArr;
 }
 
 export function getIntegers(nums) {
 	if (!nums) throw new Error('nums is required');
 	const intArr = [];
-	for (let i = 0; i < nums.length; i++) {
-		if (Number.isInteger(nums[i]))
-			intArr.push(nums[i]);
-	}
+	nums.forEach(num => {
+		if (Number.isInteger(num))
+			intArr.push(num);
+	});
 	return intArr;
 }
 
 export function getCities(users) {
 	if (!users) throw new Error('users is required');
 	const cities = [];
-	for (let i = 0; i < users.length; i++) {
-		cities.push(users[i].data.city.displayName);
-	}
+	users.forEach(user => {
+		cities.push(user.data.city.displayName);
+
+	});
 	return cities;
 }
 
 export function getSquareRoots(nums) {
 	if (!nums) throw new Error('nums is required');
 	const squareRootArr = [];
-	for (let i = 0; i < nums.length; i++)
-		squareRootArr.push(Number(Math.sqrt(nums[i]).toFixed(2)));
+	nums.forEach(num => {
+		squareRootArr.push(Number(Math.sqrt(num).toFixed(2)));
+	});
 	return squareRootArr;
 }
 
@@ -60,20 +62,20 @@ export function findSentencesContaining(sentences, str) {
 	if (!sentences) throw new Error('sentences is required');
 	if (!str) throw new Error('str is required');
 	const sentencesHavingStrArr = [];
-	for (let i = 0; i < sentences.length; i++) {
-		if (sentences[i].toLowerCase().includes(str.toLowerCase()))
-			sentencesHavingStrArr.push(sentences[i]);
-	}
+	sentences.forEach(sentence => {
+		if (sentence.toLowerCase().includes(str.toLowerCase()))
+			sentencesHavingStrArr.push(sentence);
+	});
 	return sentencesHavingStrArr;
 }
 
 export function getLongestSides(triangles) {
 	if (!triangles) throw new Error('triangles is required');
 	const longestSidesArr = [];
-	for (let i = 0; i < triangles.length; i++) {
-		const tempTriangle = triangles[i];
+	triangles.forEach(triangle => {
+		const tempTriangle = triangle;
 		tempTriangle.sort(function (a, b) { return b - a });
 		longestSidesArr.push(tempTriangle[0]);
-	}
+	});
 	return longestSidesArr;
 }
