7e9f8e1fe2ccd026dd902d6ebe25b026d6d33e31
Smitha <smitha.kotian@gmail.com>
2023-10-14 21:02:45 +0100

    feat: add testcases for getScreentimeAlertList function

--- test/exercise6-optional.test.js
+++ test/exercise6-optional.test.js
@@ -41,39 +41,137 @@ describe("createRange", () => {
   });
   test("return range of numbers as an array when step is posituve", () => {
     expect(createRange(3, 11, 2)).toEqual([3, 5, 7, 9, 11]);
-    expect(createRange(-3,12,2)).toEqual([-3,-1,1,3, 5, 7, 9, 11]);
+    expect(createRange(-3, 12, 2)).toEqual([-3, -1, 1, 3, 5, 7, 9, 11]);
   });
   test("return range of numbers as an array when step is negative", () => {
-    expect(createRange(11,3,-2)).toEqual([11,9,7,5,3]);
-    expect(createRange(-3,-12,-2)).toEqual([-3,-5,-7,-9, -11]);
+    expect(createRange(11, 3, -2)).toEqual([11, 9, 7, 5, 3]);
+    expect(createRange(-3, -12, -2)).toEqual([-3, -5, -7, -9, -11]);
   });
   test("return just the start element in the array if step is greater than difference between start and end", () => {
     expect(createRange(3, 7, 5)).toEqual([3]);
-    expect(createRange(-10,-14,-6)).toEqual([-10]);
+    expect(createRange(-10, -14, -6)).toEqual([-10]);
   });
   test("returns empty array if start is greater than end and step is positive", () => {
     expect(createRange(33, 11, 2)).toEqual([]);
-    expect(createRange(3,-12,2)).toEqual([]);
+    expect(createRange(3, -12, 2)).toEqual([]);
   });
 
   test("returns empty array if start is less than end and step is negative", () => {
     expect(createRange(3, 11, -2)).toEqual([]);
-    expect(createRange(-3,12, -2)).toEqual([]);
+    expect(createRange(-3, 12, -2)).toEqual([]);
   });
 
   test("returns empty array if step = 0", () => {
     expect(createRange(3, 11, 0)).toEqual([]);
-    expect(createRange(3,-12, 0)).toEqual([]);
+    expect(createRange(3, -12, 0)).toEqual([]);
   });
 
   test("returns empty array if start is greater than end and step is not passed", () => {
     expect(createRange(33, 11)).toEqual([]);
-    expect(createRange(3,-12)).toEqual([]);
+    expect(createRange(3, -12)).toEqual([]);
   });
 
   test("returns all elements from start to end in an array if start is less than end and step is not passed", () => {
-    expect(createRange(3, 11)).toEqual([3,4,5,6,7,8,9,10,11]);
-    expect(createRange(3,3)).toEqual([3]);
+    expect(createRange(3, 11)).toEqual([3, 4, 5, 6, 7, 8, 9, 10, 11]);
+    expect(createRange(3, 3)).toEqual([3]);
   });
 });
 
+describe("getScreentimeAlertList", () => {
+  const users = [
+    {
+      username: "beth_1234",
+      name: "Beth Smith",
+      screenTime: [
+        {
+          date: "2019-05-01",
+          usage: { twitter: 34, instagram: 22, facebook: 40 },
+        },
+        {
+          date: "2019-05-02",
+          usage: { twitter: 56, instagram: 40, facebook: 31 },
+        },
+        {
+          date: "2019-05-03",
+          usage: { twitter: 12, instagram: 15, facebook: 19 },
+        },
+        {
+          date: "2019-05-04",
+          usage: { twitter: 10, instagram: 56, facebook: 61 },
+        },
+      ],
+    },
+    {
+      username: "sam_j_1989",
+      name: "Sam Jones",
+      screenTime: [
+        {
+          date: "2019-06-11",
+          usage: { mapMyRun: 20, whatsApp: 40, facebook: 30, safari: 30 },
+        },
+        {
+          date: "2019-06-13",
+          usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 16 },
+        },
+        {
+          date: "2019-06-14",
+          usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 },
+        },
+      ],
+    },
+
+    {
+      username: "max_j_1989",
+      name: "Max Jones",
+      screenTime: [
+        {
+          date: "2019-06-11",
+          usage: { mapMyRun: 30, whatsApp: 30, facebook: 30, safari: 20 },
+        },
+        {
+          date: "2019-06-13",
+          usage: { mapMyRun: 30, whatsApp: 30, facebook: 30, safari: 10 },
+        },
+        {
+          date: "2019-06-14",
+          usage: { mapMyRun: 0, whatsApp: 0, facebook: 0, safari: 31 },
+        },
+      ],
+    },
+  ];
+
+  test("it throws an error if users array  is not passed to the function", () => {
+    expect(() => {
+      getScreentimeAlertList();
+    }).toThrow("users is required");
+  });
+  test("it throws an error if the date is not passed to the function", () => {
+    expect(() => {
+      getScreentimeAlertList(users);
+    }).toThrow("date is required");
+  });
+
+  test("return an array of usernames of users who have more than 100 minutes of screen time on a given date", () => {
+    expect(getScreentimeAlertList(users, "2019-05-04")).toEqual(["beth_1234"]);
+    expect(getScreentimeAlertList(users, "2019-06-11")).toEqual([
+      "sam_j_1989",
+      "max_j_1989",
+    ]);
+  });
+
+  test("return empty array when there is a user with usage equal to 100 but not greater than 100 for the given date", () => {
+    expect(getScreentimeAlertList(users, "2019-06-13")).toEqual([]);
+  });
+
+  test("return an empty array when there are no users with screentime greater than 100 on the given date ", () => {
+    expect(getScreentimeAlertList(users, "2019-06-14")).toEqual([]);
+  });
+
+  test("return an empty array when an empty array of users is passed ", () => {
+    expect(getScreentimeAlertList(users, "2019-06-14")).toEqual([]);
+  });
+
+  test("return an empty array when the date passed is not recorded for any user ", () => {
+    expect(getScreentimeAlertList(users, "2021-06-14")).toEqual([]);
+  });
+});
