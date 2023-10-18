d8a62fcba8d2c1e28e6b66fb10dc2223437ec2ac
smithaagarwal <smitha.kotian@gmail.com>
2023-10-18 15:25:09 +0100

    fix: refactor the code to include the review comments

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -11,22 +11,12 @@ export function isFromManchester(person) {
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
-	if (people % 40) {
-		return (Math.floor(people / 40) + 1);
-	}
-	else {
-		return (people / 40);
-	}
+	return (people % 40) ? (Math.floor(people / 40) + 1): (people / 40);
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
-	let count = 0;
-	arr.forEach(element => {
-		if (element === 'sheep')
-			count++;
-	});
-	return count;
+	return arr.filter(elem => elem === 'sheep').length;
 }
 
 export function hasMPostCode(person) {
