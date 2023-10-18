e0449ee40cb4d1bcb73d11232c693b49c00337ce
smithaagarwal <smitha.kotian@gmail.com>
2023-10-18 14:08:13 +0100

    fix: refactor the code to include the review comments

--- challenges/exercise3.js
+++ challenges/exercise3.js
@@ -1,68 +1,49 @@
 export function findSmallNums(nums) {
-	if (!nums) throw new Error('nums is required');
-	const smallerNumArr = nums.filter(num => {
-		return (num < 1);
-	});
-	return smallerNumArr;
+  if (!nums) throw new Error("nums is required");
+  return nums.filter((num) => num < 1);
 }
 
 export function findNamesBeginningWith(names, char) {
-	if (!names) throw new Error('names is required');
-	if (!char) throw new Error('char is required');
-	const namesArr = names.filter(name => {
-		return (name.startsWith(char));
-	});
-	return namesArr;
+  if (!names) throw new Error("names is required");
+  if (!char) throw new Error("char is required");
+  return names.filter((name) => name.startsWith(char));
 }
 
 export function findVerbs(words) {
-	if (!words) throw new Error('words is required');
-	const verbssArr = words.filter(word => {
-		return (word.startsWith('to '));
-	});
-	return verbssArr;
+  if (!words) throw new Error("words is required");
+  return words.filter((word) => word.startsWith("to "));
 }
 
 export function getIntegers(nums) {
-	if (!nums) throw new Error('nums is required');
-	const intArr = nums.filter(num => {
-		return (Number.isInteger(num));
-	});
-	return intArr;
+  if (!nums) throw new Error("nums is required");
+  return nums.filter((num) => Number.isInteger(num));
 }
 
 export function getCities(users) {
-	if (!users) throw new Error('users is required');
-	const cities = users.map(user => {
-		return user.data.city.displayName;
-
-	});
-	return cities;
+  if (!users) throw new Error("users is required");
+  return users.map((user) => user.data.city.displayName);
 }
 
 export function getSquareRoots(nums) {
-	if (!nums) throw new Error('nums is required');
-	const squareRootArr = nums.map(num => {
-		return (Number(Math.sqrt(num).toFixed(2)));
-	});
-	return squareRootArr;
+  if (!nums) throw new Error("nums is required");
+  return nums.map((num) => Number(Math.sqrt(num).toFixed(2)));
 }
 
 export function findSentencesContaining(sentences, str) {
-	if (!sentences) throw new Error('sentences is required');
-	if (!str) throw new Error('str is required');
-	const sentencesHavingStrArr = sentences.filter(sentence => {
-		return (sentence.toLowerCase().includes(str.toLowerCase()));
-	});
-	return sentencesHavingStrArr;
+  if (!sentences) throw new Error("sentences is required");
+  if (!str) throw new Error("str is required");
+  return sentences.filter((sentence) =>
+    sentence.toLowerCase().includes(str.toLowerCase())
+  );
 }
 
 export function getLongestSides(triangles) {
-	if (!triangles) throw new Error('triangles is required');
-	const longestSidesArr = triangles.map(triangle => {
-		const tempTriangle = [...triangle];
-		tempTriangle.sort(function (a, b) { return b - a });
-		return tempTriangle[0];
-	});
-	return longestSidesArr;
+  if (!triangles) throw new Error("triangles is required");
+  return triangles.map((triangle) => {
+    const tempTriangle = [...triangle];
+    tempTriangle.sort(function (a, b) {
+      return b - a;
+    });
+    return tempTriangle[0];
+  });
 }
